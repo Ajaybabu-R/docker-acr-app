afbec698719ed73caa465450e6f6e4c9dc63872f
Ajay Babu <ajayar536@gmail.com>
2026-02-24 15:34:01 +0530

    Test CI deployment

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ MODEL_VERSION = os.getenv("MODEL_VERSION", "v1")
 @app.get("/")
 def read_root():
     return {
-        "message": "Hello World 🚀",
+        "message": "Hello World v2 🚀",
         "model_version": MODEL_VERSION
     }
 
